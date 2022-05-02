a822650849fd90c326c6c1fb1365cf88efbfb5e2
dk-raw <ddkatsios@protonmail.com>
2022-05-02 11:23:24 +0300

    changes

--- pages/fleet.vue
+++ pages/fleet.vue
@@ -23,15 +23,15 @@
           header="Lockheed F-16C/D/V Block 30/40/50/52+/72 Viper"
           description="State-of-the-art supersonic air superiority fighter armed with AIM-9 Sidewinders, AIM-120 AMRAAMs, AIM-2000s infra red guided, and AGM 88 anti-radiation missiles. It is also equipped with an AN/APG-83 AESA radar, and electronic warfare countermeasures."
           imageUrl="f16-viper.jpg"
-          :tags="[{ type: 'Fighter' }]"
+          :tags="[{ type: 'Fighter' }, { type: 'Multi-role' }]"
         />
       </div>
       <div class="col">
         <Card
           header="Dassault Rafale B/C F3R"
-          description="Two seat, twin engine air supremacy fighter jet equipped with a SAGEM-OSF optronique secteur frontal infra red search and track system, RBE2-AA AESA radar and armed with MICA EM and MAGIC-2 infra red active radar data link guided missles as well as MBDA AM 39-Exocet anti-ship missiles."
+          description="4.5 gen air supremacy fighter jet equipped with a SAGEM-OSF optronique secteur frontal IRIS-T system, a long-range RBE2-AA AESA radar and armed with 4 METEOR BVRAAM datalink missiles, 2 SCALP-EGs cruise missiles and AM-39 EXOCET blk 2 anti-ship missiles."
           imageUrl="rafale.jpg"
-          :tags="[{ type: 'Fighter' }]"
+          :tags="[{ type: 'Fighter' }, { type: 'Multi-role' }]"
         />
       </div>
       <div class="col">
@@ -39,18 +39,14 @@
           header="McDonnell Douglas F-4E Phantom II PI 2000"
           description="Long range supersonic fighter bomber armed with an AFDS, CBU-58/71 bombs, Mk 36 Destructor bombs, AGM-65 G infra red, AGM-65 A/B guided missles and GBU-10-12-16 laser guided missles."
           imageUrl="f4-phantom.jpg"
-          :tags="[
-            { type: 'Bomber' },
-            { type: 'Interceptor' },
-            { type: 'Attack' },
-          ]"
+          :tags="[{ type: 'Bomber' }, { type: 'Attack' }]"
         />
       </div>
       <div class="col">
         <Card
-          header="Dassault Mirage 2000-5"
-          description="Equipped with the Antilope 50 radar, the Damocles forward looking infra red and Thales RDY-2 synthetic aperture radar and armed with MICA EM/MAGIC-2 infra red active radar data link guided missles, ΑΜ-39 EXOCET active radar bombs and 2 SCALP-EG augmenting charge bombs."
-          imageUrl="mirage2000.jpg"
+          header="Dassault Mirage 2000-5 Mk II"
+          description="Equipped with the Antilope 50 radar, the Damocles forward looking infra red and Thales RDY-2 synthetic aperture radar and armed with MICA EM/MAGIC-2 infra red active radar data link guided missles, ΑΜ-39 EXOCET anti-ship missiles and 2 SCALP-EG augmenting charge bombs."
+          imageUrl="mirage2000-5.jpg"
           :tags="[{ type: 'Fighter' }]"
         />
       </div>
@@ -59,23 +55,15 @@
           header="Lockheed Martin F-35 Lightning II"
           description="Soon to be equipped with a APG-81 AESA radar, AAQ-40 E/O targeting system and ASQ-239 Barracuda electronic warfare system and armed with a GAU-22/A 4-barrel rotary cannon, 2 ΑΙΜ-120 B active radar guided missles, 4 AIM-9 Sidewinder missles and 2 AGM-154 infra red bombs."
           imageUrl="f35.jpg"
-          :tags="[{ type: 'Fighter' }, { type: 'Bomber' }, { type: 'Stealth' }]"
-        />
-      </div>
-      <div class="col">
-        <Card
-          header="Lockheed P-3B Orion"
-          description="Fitted with a ASQ-81 magnetic anomaly detector, a AN/APS-115 maritime surveillance radar and AN/APS-137D(V)5 inverse synthetic aperture search radar. The Orion is also armed with Mk 46 Mod.5 torpedoes and Mk 55 naval mines for optimal submarine hunting and naval surveillance."
-          imageUrl="p-3.jpg"
-          :tags="[{ type: 'Support' }, { type: 'Anti-Submarine' }]"
+          :tags="[{ type: 'Fighter' }, { type: 'Multi-role' }, { type: 'TBA' }]"
         />
       </div>
       <div class="col">
         <Card
           header="Erieye EMB-145H AEW&C"
           description="Airborne early warning and control (Command & Control) aircraft fitted with an active electronically scanned array radar, anti-submarine warfare system and forward looking infra red system. Capable of data link & voice connections, ELINT intelligence gathering and EMCON."
-          imageUrl="emb-145h.jpg"
-          :tags="[{ type: 'Support' }, { type: 'AEW&C' }]"
+          imageUrl="emb-145h-awacs.jpg"
+          :tags="[{ type: 'AWACS' }]"
         />
       </div>
       <div class="col">
@@ -83,11 +71,7 @@
           header="Lockheed C-130/H Hercules"
           description="Military transport aircraft capable of transporting heavy payloads over large distances and takeoffs or landings on unprepared and unpaved runways. The Hellenic Air Force operates 2 C-130H EW variants used for electronic warfare and early warning and control."
           imageUrl="c-130.jpg"
-          :tags="[
-            { type: 'Support' },
-            { type: 'Transport' },
-            { type: 'AEW&C' },
-          ]"
+          :tags="[{ type: 'Tactical Transport' }, { type: 'EW' }]"
         />
       </div>
       <div class="col">
@@ -95,7 +79,27 @@
           header="Alenia C-27J Spartan"
           description="Military transport aircraft capable of transporting heavy payloads over large distances. It's maximum is payload weight is around 11,6 tons and features 2 Rolls-Royce AE2100-D2A turboprop engines wwith a combined thrust of 6,916 kW and a 1,759 km range."
           imageUrl="c-27j.jpg"
-          :tags="[{ type: 'Support' }, { type: 'Transport' }]"
+          :tags="[{ type: 'Tactical Transport' }]"
+        />
+      </div>
+      <div class="col">
+        <Card
+          header="Lockheed P-3B Orion"
+          description="Fitted with a ASQ-81 magnetic anomaly detector, a AN/APS-115 maritime surveillance radar and AN/APS-137D(V)5 inverse synthetic aperture search radar. The Orion is also armed with Mk 46 Mod.5 torpedoes and Mk 55 naval mines for optimal submarine hunting and naval surveillance."
+          imageUrl="p-3b.jpg"
+          :tags="[{ type: 'Navy Air Force' }, { type: 'Anti-Submarine' }]"
+        />
+      </div>
+      <div class="col">
+        <Card
+          header="Sikorsky SH-70B6/MH-60R Aegean Hawk"
+          description="Multi role helicopter used for anti-submarine and anti-surface warfare. Equipped with a pneumatic sonobuoy launcher, a magnetic anomaly detector and forward looking infra red. Armed with MK-46 torpedoes and AGM-119B/AGM-114M guided missles."
+          imageUrl="sh-70.jpg"
+          :tags="[
+            { type: 'Navy Air Force' },
+            { type: 'Helicopter' },
+            { type: 'Anti-Submarine' },
+          ]"
         />
       </div>
       <div class="col">
@@ -106,8 +110,7 @@
           :tags="[
             { type: 'Army Air Force' },
             { type: 'Helicopter' },
-            { type: 'Support' },
-            { type: 'Transport' },
+            { type: 'Tactical Transport' },
           ]"
         />
       </div>
@@ -120,18 +123,6 @@
             { type: 'Army Air Force' },
             { type: 'Helicopter' },
             { type: 'Attack' },
-          ]"
-        />
-      </div>
-      <div class="col">
-        <Card
-          header="Sikorsky SH-70B6/MH-60R Aegean Hawk"
-          description="Multi role helicopter used for anti-submarine and anti-surface warfare. Equipped with a pneumatic sonobuoy launcher, a magnetic anomaly detector and forward looking infra red. Armed with MK-46 torpedoes and AGM-119B/AGM-114M guided missles."
-          imageUrl="sh-70.jpg"
-          :tags="[
-            { type: 'Navy' },
-            { type: 'Helicopter' },
-            { type: 'Anti-Submarine' },
           ]"
         />
       </div>
@@ -153,7 +144,11 @@
           header="NHIndustries NH90 TTH"
           description="Multi role utility military helicopter primarily used for tactical transport fitted with a full fly-by-wire system, infrared suppression system, wire strike protection system, forward looking infrared, helmet mounted display. The NH90 possesses the lowest radar signature in its class."
           imageUrl="nh-90.jpg"
-          :tags="[{ type: 'Helicopter' }, { type: 'Multi Role' }]"
+          :tags="[
+            { type: 'Army Air Force' },
+            { type: 'Helicopter' },
+            { type: 'Tactical Transport' },
+          ]"
         />
       </div>
       <div class="col">
@@ -177,7 +172,7 @@
           header="General Atomics MQ-9 Reaper"
           description="Unmaned combat aerial vehicle with the a maximum payload of 1,700kg and a maximum speed of 260 knots. It can be armed with up to 8 AGM-114 ATGMs or 4 GBU-12 laser guided bombs. The MQ-9 also features an AN/DAS-1 MTS-B electro-optical targeting system, AN/APY-8 Lynx II radar, SATCOM and autonomous flight."
           imageUrl="mq-9.jpg"
-          :tags="[{ type: 'UAV' }, { type: 'UCAV' }]"
+          :tags="[{ type: 'UAV' }, { type: 'UCAV' }, { type: 'TBA' }]"
         />
       </div>
       <div class="col">
@@ -202,7 +197,7 @@
       </div>
       <div class="col">
         <Card
-          header="E1-79 Pegasus II"
+          header="HAI Pegasus II"
           description="Long endurance over-battlefield reconnaissance and intelligence surveillance unmaned aerial vehicle also capable fo ELINT. The Pegasus has a 15 hour range and can easily operate over the battlefield."
           imageUrl="pegasus.jpg"
           :tags="[{ type: 'UAV' }, { type: 'Reconnaissance' }]"
@@ -210,26 +205,34 @@
       </div>
       <div class="col">
         <Card
-          header="Raytheon MIM-104 Patriot PAC3"
-          description="Remotely controled HSAM with an 150km interception range. Capable of intercepting up to 9 ballistic, tactical, cruise missles or fighter jets. It can be armed either with MIM-104D GEM/MIM-104B SOJC or MIM-104A supersonic 90kg warhead missles. It is also equipped with a AN/MPQ-65 long range radar set."
-          imageUrl="mim-patriot.jpg"
-          :tags="[{ type: 'HSAM' }, { type: 'ABM' }]"
+          header="MIM-104 Patriot PAC-3"
+          description="Remotely controled SAM with a 150km range. Capable of intercepting up to 9 ballistic, tactical, cruise missles or aircraft at a time. It can be armed either with MIM-104D/B SOJC or MIM-104A supersonic 90kg warhead missles. It is also equipped with a AN/MPQ-65 AESA long range radar set."
+          imageUrl="patriot-pac3.jpg"
+          :tags="[{ type: 'SAM' }]"
         />
       </div>
       <div class="col">
         <Card
-          header="NPO Almaz S-300 PMU1"
-          description="HSAM capable of protecting a large area by intercepting up to 4 fighter jets or ballistic, tactical, cruise missles at a time. Equipped with RSR and SSR radars and armed with 4 long range 48Ν6Ε TVM 150kg warhead missles."
-          imageUrl="s-300.jpg"
-          :tags="[{ type: 'HSAM' }, { type: 'ABM' }]"
+          header="S-300 PMU1"
+          description="HSAM capable of protecting a large area by intercepting up to 4 fighter jets or ballistic, tactical, cruise missles at a time at a range of 120km. It is equipped with RSR and SSR radars and armed with 4 long range 48Ν6Ε TVM 150kg warhead missles per launcher."
+          imageUrl="s300-pmu1.jpg"
+          :tags="[{ type: 'SAM' }]"
         />
       </div>
       <div class="col">
         <Card
-          header="Thomson CSF Matra Crotale NG"
+          header="Skyguard VELOS"
+          description="Short range, all-weather, air defence system composed of 4 RIM-7M Sparrow missile launchers, 4 twin 35mm Oerlikon GDF guns, a SUPER GIRAFFE search radar and 2 SKYGUARD track radars. The VELOS can engage up to 6 targets a time at a range of 19km."
+          imageUrl="velos.jpg"
+          :tags="[{ type: 'SHORAD' }, { type: 'SAM' }]"
+        />
+      </div>
+      <div class="col">
+        <Card
+          header="Crotale NG"
           description="Infra red differential ecartometry short range anti-ship and anti-aircraft system armed with VT-1 supersonic 18kg warhead missles and equipped with a S-band pulse doppler radar and Ku-band TWT tracking radar. Thermal camera, CCD camera and IR Localizer are fitted for optimal missle interception."
           imageUrl="crotale-ng.jpg"
-          :tags="[{ type: 'SHORAD' }, { type: 'ASM' }]"
+          :tags="[{ type: 'SHORAD' }, { type: 'SAM' }]"
         />
       </div>
       <div class="col">
@@ -237,15 +240,7 @@
           header="TOR M1"
           description="High accuracy short range SAM caplable of destroying airplanes, helicopters, cruise missiles, precision guided munitions, UAVs and short range ballistic threats. Armed with 4 15kg warhead radar controlled proximity fuzed missles. Capable of intercepting up to 2 targets simultaneously."
           imageUrl="tor-m1.jpg"
-          :tags="[{ type: 'SHORAD' }, { type: 'ASM' }]"
-        />
-      </div>
-      <div class="col">
-        <Card
-          header="9K33 Osa"
-          description="High accuracy short range tactical SAM equipped with a 1S51M3-2 radar consisting of a J band parabolic dish antenna, an elliptical rotating surveillance antenna and a large pulsed J band engagement antenna making it ideal for short range applications."
-          imageUrl="9k33-osa.jpg"
-          :tags="[{ type: 'SHORAD' }, { type: 'ASM' }]"
+          :tags="[{ type: 'SHORAD' }, { type: 'SAM' }]"
         />
       </div>
     </div>
@@ -260,15 +255,19 @@
 <script>
 export default {
   name: "Fleet",
+  loading: {
+    color: "red",
+    height: "5px",
+  },
   data() {
     return {
       fleet: [
         {
-          name: "Lockheed F-16C/D/V Block 52+/72 Viper",
+          name: "Lockheed F-16C/D/V Block 30/40/50/52+/72 Viper",
           description:
             "State-of-the-art supersonic air superiority fighter armed with AIM-9 Sidewinders, AIM-120 AMRAAMs, AIM-2000s infra red guided, and AGM 88 anti-radiation missiles. It is also equipped with an AN/APG-83 AESA radar, and electronic warfare countermeasures.",
-          imageUrl: "f-16.jpg",
-          tags: [{ type: "Fighter" }],
+          imageUrl: "f-16-viper.jpg",
+          tags: [{ type: "Fighter" }, { type: "Bomber" }],
         },
       ],
     };
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,17 @@
       <p class="lead fst-italic mb-0">-Αἰὲν Ὑψικρατεῖν</p>
       <small>"Always Dominate the Heights"</small>
       <hr />
-      <video
+      <div class="row">
+        <div class="col-md">
+          <img src="../assets/images/home.jpg" alt="" class="w-100" />
+        </div>
+        <div class="col-md">
+          <p class="lead fs-4 mt-4 mt-md-0">
+            Best Air Force in NATO for 5 years straight and still counting...
+          </p>
+        </div>
+      </div>
+      <!-- <video
         src="../assets/videos/haf-promo.mp4"
         type="video/webm"
         autoplay="true"
@@ -13,7 +23,7 @@
         loop
         class="rounded border border-2"
         style="width: 100%; height: 100%; opacity: 0.8"
-      ></video>
+      ></video> -->
     </div>
   </div>
 </template>
